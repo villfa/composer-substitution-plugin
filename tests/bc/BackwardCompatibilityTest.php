00bfa824c2fa9856efa19b29cc6a42135a79e9bb
Fabien Villepinte <fabien.villepinte@gmail.com>
2020-07-23 16:01:34 +0200

    Test with Composer 1.10.9

--- tests/bc/BackwardCompatibilityTest.php
+++ tests/bc/BackwardCompatibilityTest.php
@@ -101,7 +101,7 @@ class BackwardCompatibilityTest extends BaseTestCase
     {
         $versions = array(
             array('snapshot'),
-            array('1.10.8'),
+            array('1.10.9'),
             array('1.9.3'),
             array('1.8.6'),
             array('1.7.3'),
