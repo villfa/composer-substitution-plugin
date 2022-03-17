8a96efac7e239c04c94ebb885d6da8a0cd10cefd
Fabien Villepinte <fabien.villepinte@gmail.com>
2022-03-17 13:41:27 +0000

    Test with Composer 1.10.25 and 2.2.9

--- tests/bc/BackwardCompatibilityTest.php
+++ tests/bc/BackwardCompatibilityTest.php
@@ -100,10 +100,10 @@ class BackwardCompatibilityTest extends BaseTestCase
     public function provideComposerVersions()
     {
         $versions = array(
-            array('2.2.7'),
+            array('2.2.9'),
             array('2.1.14'),
             array('2.0.14'),
-            array('1.10.24'),
+            array('1.10.25'),
         );
 
         if (version_compare(PHP_VERSION, '8.0') < 0) {
