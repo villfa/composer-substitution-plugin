16df4470da47b78561a33abbfacd477d6b33831c
Fabien Villepinte <fabien.villepinte@gmail.com>
2024-01-31 11:22:34 +0100

    Test with Composer 2.6.6, 2.5.8 and 2.2.22

--- tests/bc/BackwardCompatibilityTest.php
+++ tests/bc/BackwardCompatibilityTest.php
@@ -103,10 +103,11 @@ class BackwardCompatibilityTest extends BaseTestCase
     public function provideComposerVersions()
     {
         $allVersions = array(
-            '2.5.5' => array('7.2.5', null),
+            '2.6.6' => array('7.2.5', null),
+            '2.5.8' => array('7.2.5', null),
             '2.4.4' => array('7.2.5', null),
             '2.3.10' => array('7.2.5', null),
-            '2.2.21' => array('5.3', null),
+            '2.2.22' => array('5.3', null),
             '2.1.14' => array('5.3', null),
             '2.0.14' => array('5.3', null),
             '1.10.26' => array('5.3', null),
